f6b1fc9417f7ee34ba5313608879a0d2d22ba9fc
asif ahemmed <asifahemmed0@gmail.com>
2025-03-14 20:38:24 +0600

    header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <header className="bg-white flex justify-center items-center py-4 gap-3">
+            <img className="w-10 h-10" src="/src/assets/chef-claude-icon.png" alt="logo" />
+            <h1 className="text-3xl">Chef Claude</h1>
+        </header>
+    );
+};
+
+export default Header;
